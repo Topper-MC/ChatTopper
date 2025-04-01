ea601ba66062d43036f6b8e0804d1b85dc5b98b4
hsgamer <huynhqtienvtag@gmail.com>
2025-04-01 20:35:29 +0700

    only use flat storage

--- src/main/java/me/hsgamer/chattopper/ChatTopper.java
+++ src/main/java/me/hsgamer/chattopper/ChatTopper.java
@@ -3,15 +3,12 @@ package me.hsgamer.chattopper;
 import io.github.projectunified.minelib.plugin.base.BasePlugin;
 import io.github.projectunified.minelib.plugin.command.CommandComponent;
 import me.hsgamer.chattopper.command.GetWordTopCommand;
-import me.hsgamer.chattopper.config.MainConfig;
 import me.hsgamer.chattopper.hook.PlaceholderHook;
 import me.hsgamer.chattopper.listener.ChatListener;
 import me.hsgamer.chattopper.manager.HolderManager;
 import me.hsgamer.chattopper.manager.StorageManager;
 import me.hsgamer.chattopper.manager.WordQueryForward;
 import me.hsgamer.chattopper.manager.WordQueryManager;
-import me.hsgamer.hscore.bukkit.config.BukkitConfig;
-import me.hsgamer.hscore.config.proxy.ConfigGenerator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,8 +17,6 @@ public final class ChatTopper extends BasePlugin {
     @Override
     protected List<Object> getComponents() {
         return Arrays.asList(
-                ConfigGenerator.newInstance(MainConfig.class, new BukkitConfig(this)),
-
                 new StorageManager(this),
                 new HolderManager(this),
 
