a5c83a392905941b1daa41e1d434b97727a2d31e
hsgamer <huynhqtienvtag@gmail.com>
2025-04-01 12:24:27 +0700

    forgot listener

--- src/main/java/me/hsgamer/chattopper/ChatTopper.java
+++ src/main/java/me/hsgamer/chattopper/ChatTopper.java
@@ -5,6 +5,7 @@ import io.github.projectunified.minelib.plugin.command.CommandComponent;
 import me.hsgamer.chattopper.command.GetWordTopCommand;
 import me.hsgamer.chattopper.config.MainConfig;
 import me.hsgamer.chattopper.hook.PlaceholderHook;
+import me.hsgamer.chattopper.listener.ChatListener;
 import me.hsgamer.chattopper.manager.HolderManager;
 import me.hsgamer.chattopper.manager.StorageManager;
 import me.hsgamer.chattopper.manager.WordQueryForward;
@@ -24,6 +25,7 @@ public final class ChatTopper extends BasePlugin {
                 new StorageManager(this),
                 new HolderManager(this),
 
+                new ChatListener(this),
                 new CommandComponent(this, new GetWordTopCommand(this)),
 
                 new WordQueryManager(this),
