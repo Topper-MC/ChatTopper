ae9b8682e2226518cfd2a82f3bcca30e2be9a9e7
hsgamer <huynhqtienvtag@gmail.com>
2025-04-01 11:39:00 +0700

    init

---
+++ src/main/java/me/hsgamer/chattopper/ChatTopper.java
@@ -0,0 +1,17 @@
+package me.hsgamer.chattopper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatTopper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
